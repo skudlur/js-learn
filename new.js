9622f7c5420e8a0c514117e627734d6d9df8f75c
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-11-17 21:54:40 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,2 +1,11 @@
 let outStr = "Hello";
 console.log(outStr);
+
+let funcOutStr = ``;
+
+function firstFunc(args) {
+  args.forEach(function(arg) {
+    funcOutStr += `Hello, ${arg}!`;
+  });
+  return funcOutStr;
+}
